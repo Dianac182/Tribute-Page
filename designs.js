de161d6fac95c6a42321d7c80e8b06d942efb2d2
Unknown <dianac182@gmail.com>
2018-04-27 13:58:49 +0200

    Make resposive behavior with bootstrap

---
+++ designs.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    const myNewHeaders = $('.new-headers');
+
+    $(window).on('resize', function() {
+        if ($(window).width() >= 992) {
+            myNewHeaders.remove();
+        } else {
+            myNewHeaders.insertAfter('.my-headers');
+        }
+    });
+});
